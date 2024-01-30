4068a2570eb9925c1630d96195da49f6a522783b
cindy <risqianacindi@gmail.com>
2024-01-30 11:07:04 +0700

    first commit

---
+++ ecommerce-website-php/cetak.php
@@ -0,0 +1,8 @@
+<?php
+
+include("includes/db.php");
+include("includes/header.php");
+include("functions/functions.php");
+
+
+?>
---
+++ ecommerce-website-php/customer/my_orders.php
@@ -0,0 +1,126 @@
+
+<center><!-- center Starts -->
+
+<h1>My Order</h1>
+
+<p class="lead"> Your Orders in one place</p>
+
+<p class="text-muted" >
+
+If you have any questions please don't hesitate to<a href="../contact.php" > contact us</a> our customer service center works for you.
+
+
+</p>
+
+
+</center><!-- center Ends -->
+
+<hr>
+
+<div class="table-responsive" ><!-- table-responsive Starts -->
+
+<table class="table table-bordered table-hover" ><!-- table table-bordered table-hover Starts -->
+
+<thead><!-- thead Starts -->
+
+<tr>
+
+<th>#</th>
+<th>Price</th>
+<th>Order code</th>
+<th>Purchase Amount</th>
+<th>Size</th>
+<th>Order date</th>
+<th>Status</th>
+<th>Action</th>
+
+
+</tr>
+
+</thead><!-- thead Ends -->
+
+<tbody><!--- tbody Starts --->
+
+<?php
+
+$customer_session = $_SESSION['customer_email'];
+
+$get_customer = "select * from customers where customer_email='$customer_session'";
+
+$run_customer = mysqli_query($con,$get_customer);
+
+$row_customer = mysqli_fetch_array($run_customer);
+
+$customer_id = $row_customer['customer_id'];
+
+$get_orders = "select * from customer_orders where customer_id='$customer_id'";
+
+$run_orders = mysqli_query($con,$get_orders);
+
+$i = 0;
+
+while($row_orders = mysqli_fetch_array($run_orders)){
+
+$order_id = $row_orders['order_id'];
+
+$due_amount = $row_orders['due_amount'];
+
+$invoice_no = $row_orders['invoice_no'];
+
+$qty = $row_orders['qty'];
+
+$size = $row_orders['size'];
+
+$order_date = substr($row_orders['order_date'],0,11);
+
+$order_status = $row_orders['order_status'];
+
+$i++;
+
+if($order_status=='pending'){
+
+$order_status = "<b style='color:red;'>Belum Dibayar</b>";
+
+}
+else{
+
+$order_status = "<b style='color:green;'>Dibayar</b>";
+
+}
+
+?>
+
+<tr><!-- tr Starts -->
+
+<th><?php echo $i; ?></th>
+
+<td>$<?php echo $due_amount; ?></td>
+
+<td><?php echo $invoice_no; ?></td>
+
+<td><?php echo $qty; ?></td>
+
+<td><?php echo $size; ?></td>
+
+<td><?php echo $order_date; ?></td>
+
+<td><?php echo $order_status; ?></td>
+
+<td>
+<a href="confirm.php?order_id=<?php echo $order_id; ?>" target="blank" class="btn btn-success btn-xs" > Konfirmasi Jika Sudah Dibayar </a>
+</td>
+
+
+</tr><!-- tr Ends -->
+
+<?php } ?>
+
+</tbody><!--- tbody Ends --->
+
+
+</table><!-- table table-bordered table-hover Ends -->
+
+</div><!-- table-responsive Ends -->
+
+
+
